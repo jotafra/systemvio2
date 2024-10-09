0e8aed4718597b9e0b85abb73a0c1be648c0330a
jotafra <jozinbrbrasil@gmail.com>
2024-10-09 16:28:36 -0300

    Crud com Querys sql

--- api/src/controllers/userController.js
+++ api/src/controllers/userController.js
@@ -1,73 +1,96 @@
-
-let users = []; 
+const connect = require("../db/connect");
 
 module.exports = class userController {
   static async createUser(req, res) {
     const { cpf, email, password, name } = req.body;
 
     if (!cpf || !email || !password || !name) {
-      return res.status(400).json({ error: "Todos os campos devem ser preenchidos" });
+      return res
+        .status(400)
+        .json({ error: "Todos os campos devem ser preenchidos" });
     } else if (isNaN(cpf) || cpf.length !== 11) {
-      return res.status(400).json({ error: "CPF inválido. Deve conter exatamente 11 dígitos numéricos" });
+      return res.status(400).json({
+        error: "CPF inválido. Deve conter exatamente 11 dígitos numéricos",
+      });
     } else if (!email.includes("@")) {
       return res.status(400).json({ error: "Email inválido. Deve conter @" });
+    } else {
+      // Construção da Query INSERT
+      const query = `INSERT INTO usuario (cpf, password, email, name ) VALUES(
+      '${cpf}',
+      '${password}',
+      '${email}',
+      '${name}')`;
+      // Executando a query criada
+      try {
+        connect.query(query, function (err) {
+          if (err) {
+            console.log(err);
+            console.log(err.code);
+            if (err.code === "ER_DUP_ENTRY") {
+              return res
+                .status(400)
+                .json({ error: "O Emailjá está vinculado a outro usuário" });
+            } else {
+              return res
+                .status(500)
+                .json({ error: "Erro interno do servidor" });
+            }
+          } else {
+            return res
+              .status(201)
+              .json({ message: "Usuário criado com sucesso" });
+          }
+        });
+      } catch (error) {
+        console.error(error);
+        res.status(500).json({ error: "Erro interno do servidor" });
+      }
     }
-
-    // Verifica se já existe um usuário com o mesmo CPF
-    const existingUser = users.find(user => user.cpf === cpf);
-    if (existingUser) {
-      return res.status(400).json({ error: "CPF já cadastrado" });
-    }
-
-    // Cria e adiciona novo usuário
-    const newUser = { cpf, email, password, name };
-    users.push(newUser);
-
-    return res.status(201).json({ message: "Usuário criado com sucesso", user: newUser });
   }
-
 
   static async getAllUsers(req, res) {
-    return res.status(200).json({ message: "Obtendo todos os usuários", users });
+    return res.status(200).json({ message: "Obtendo todos os usuários" });
   }
-
-
   static async updateUser(req, res) {
     // Desestrutura e recupera os dados enviados via corpo da requisição
-    const {cpf,email,password,name} = req.body;
+    const { cpf, email, password, name } = req.body;
 
     // Validar se todos os campos foram preenchidos
-    if(!cpf || !email || !password || !name) {
-        return res.status(400).json({error:"Todos os campos devem ser preenchidos"});
+    if (!cpf || !email || !password || !name) {
+      return res.status(400).json({ error: "Todos os campos devem ser preenchidos" });
     }
     //Procurar o indice do user no Array 'users' pelo cpf
-    const userIndex = users.findIndex(user => user.cpf === cpf)
+    const userIndex = users.findIndex((user) => user.cpf === cpf);
+
     //Se o usuário não for encontrado userIndex equivale a -1
-    if(userIndex === -1){
-        return res.status(400).json({error: "Usuário não encontrado"});
+    if (userIndex === -1) {
+      return res.status(400).json({ error: "Usuário não encontrado" });
     }
 
     //Atualiza os dados do usuário no Array 'users'
-    users[userIndex] = {cpf, email, password, name}
-    
-    return res.status(200).json({message: "Usuário atualizado", user:users[userIndex]})
+    users[userIndex] = { cpf, email, password, name };
+
+    return res
+      .status(200)
+      .json({ message: "Usuário atualizado", user: users[userIndex] });
   }
 
   static async deleteUser(req, res) {
     // Obtem o parametro 'id' da requisição, que é o cpf do user a ser deletado
-    const userId = req.params.cpf
+    const userId = req.params.cpf;
 
     //Procurar o indice do user no Array 'users' pelo cpf
-    const userIndex = users.findIndex(user => user.cpf === userId)
+    const userIndex = users.findIndex((user) => user.cpf === userId);
+
     //Se o usuário não for encontrado userIndex equivale a -1
-    if(userIndex === -1){
-        return res.status(400).json({error: "Usuário não encontrado"});
+    if (userIndex === -1) {
+      return res.status(400).json({ error: "Usuário não encontrado" });
     }
 
     // Removendo o usuário do Array 'users'
-    users.splice(userIndex,1);
+    users.splice(userIndex, 1);
 
-    return res.status(200).json({message: "Usuário Apagado!"})
-   
+    return res.status(200).json({ message: "Usuário Apagado!" });
+  }
 };
-}
